235de8d2bee0dca1e99745ce0ce9df17af6aac2b
mad <marc@balloonary.io>
2021-05-18 20:53:04 +0200

    .

--- data.js
+++ data.js
@@ -6,5 +6,45 @@ data = [
 			'Jake the Dog',
 			'Bill Cipher',
 		]
-	}
+	},
+	{
+		filename: 'Jesper.png',
+		options: [
+			'Jesper',
+			'Kaz',
+			'Bill Cipher',
+		]
+	},
+	{
+		filename: 'Kaz.png',
+		options: [
+			'Kaz',
+			'Jesper',
+			'Bill Cipher',
+		]
+	},
+	{
+		filename: 'Inej.png',
+		options: [
+			'Inej',
+			'Jake the Dog',
+			'Bill Cipher',
+		]
+	},
+	{
+		filename: 'seahawk.png',
+		options: [
+			'Seahawk',
+			'She-Ra',
+			'Bill Cipher',
+		]
+	},
+	{
+		filename: 'Bill Cipher.jpg',
+		options: [
+			'Bill Cipher',
+			'Seahawk',
+			'She-Ra',
+		]
+	},
 ];
